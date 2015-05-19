dbc993ff3401442d499f6dbd288fd772427aa73a
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-19 13:35:30 -0700

    more edits

--- src/solvers.js
+++ src/solvers.js
@@ -14,17 +14,54 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var arr = [];
+  for(var i = 0; i < n; i++){
+    var row = [];
+    for(var j = 0; j < n; j++){
+      if(i===j){
+        row.push(1);
+      } else{
+        row.push(0);
+      }
+    }
+    arr.push(row);
+  }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(arr));
+  return arr;
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var solutions = [];
+   var makeEmptyMatrix = function(n) {
+    return _(_.range(n)).map(function() {
+      return _(_.range(n)).map(function() {
+        return 0;
+      });
+    });
+  };
+   var recurse = function(board, i){
+    var skip = i;
+    for(var j = 1; j < n; j++){
+      if(i === board.get('n')-1){
+        i = 0;
+      }
+      board.toggle(j, i+1);
+      i++;
+    }
+    if(board)
+      solutions.push(board);
+      solutionCount++;
+    };
+    //if solution is valid AND pieces === n
+  for(var i = 0; i < n; i++){
+    var board = makeEmptyMatrix(n);
+    board.toggle(0, i);
+    recurse(board, i);
+  }
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -48,3 +85,39 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+/*var newBoard = [[0, 0, 0, 0]];
+  var solutions = [];
+  var three = [
+    [[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]],
+    [[0, 0, 1],
+     [0, 1, 0],
+     [1, 0, 0]],
+    [[1, 0, 0],
+     [0, 0, 1],
+     [0, 1, 0]],
+    [[0, 0, 1],
+     [1, 0, 0],
+     [0, 1, 0]],
+    [[0, 1, 0],
+     [1, 0, 0],
+     [0, 0, 1]],
+    [[0, 1, 0],
+     [0, 0, 1],
+     [1, 0, 0]]
+  ];
+
+  for (var i = 0; i<n; i++) {
+    newBoard[0][i]=1;
+    if(i>0){
+      newBoard[0][i-1]=0;
+    }
+    var a = [newBoard[0]];
+    for(var j = 1; j <= three.length; j++){
+      a[j][i]=0;
+      a[j][])
+    }
+    solutions.push()
+  }*/
