76c8ea74475cd1b8218713ae92419334f9fab7b0
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-19 17:27:06 -0700

    GOT N-ROOKS (and almost kinda queens...)

--- src/solvers.js
+++ src/solvers.js
@@ -35,34 +35,35 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = 0;
-  var solutions = [];
-   var makeEmptyMatrix = function(n) {
-    return _(_.range(n)).map(function() {
-      return _(_.range(n)).map(function() {
-        return 0;
-      });
-    });
-  };
-   var recurse = function(board, i){
-    var skip = i;
-    for(var j = 1; j < n; j++){
-      if(i === board.get('n')-1){
-        i = 0;
-      }
-      board.toggle(j, i+1);
-      i++;
-    }
-    if(board)
-      solutions.push(board);
-      solutionCount++;
-    };
-    //if solution is valid AND pieces === n
-  for(var i = 0; i < n; i++){
-    var board = makeEmptyMatrix(n);
-    board.toggle(0, i);
-    recurse(board, i);
+  if(n<3){
+    return n;
   }
-
+  var myBoard = new Board({'n':n});
+  var makeSolution = function(board, p){
+    for(var i = 0; i < n; i++){
+      //make sure current board is valid, because prev. correct has pieces
+      if(i > 0){
+        board.rows()[p][i-1] = 0;
+        for(var j = p+1; j < n; j++){
+          for(var k = 0; k < n; k++){
+            board.rows()[j][k] = 0;
+          }
+        }
+      }
+      //make decision
+      board.rows()[p][i] = 1;
+      if(board.hasAnyRooksConflicts()){
+        board.rows()[p][i] = 0;
+      } else{
+        if(p+1 === n){
+          solutionCount++;
+        } else{
+          makeSolution(board, p+1);
+        }
+      }
+    }
+  };
+  makeSolution(myBoard, 0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
@@ -80,8 +81,42 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
+  var solutionCount = 0; //fixme
+  if(n<2){
+    return 1;
+  }
+  if(n<4){
+    return 0;
+  }
+  var storage = [];
+  var myBoard = new Board({'n':n});
+  var makeSolution = function(board, p){
+    for(var i = 0; i < n; i++){
+      //make sure current board is valid, because prev. correct has pieces
+      if(i > 0){
+        board.rows()[p][i-1] = 0;
+        for(var j = p+1; j < n; j++){
+          for(var k = 0; k < n; k++){
+            board.rows()[j][k] = 0;
+          }
+        }
+      }
+      //make decision
+      board.rows()[p][i] = 1;
+      if(board.hasAnyQueensConflicts()){
+        board.rows()[p][i] = 0;
+      } else{
+        if(p+1 === n){
+          solutionCount++;
+          storage.push(board);
+        } else{
+          makeSolution(board, p+1);
+        }
+      }
+    }
+  };
+  makeSolution(myBoard, 0);
+  console.log(storage);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
@@ -121,3 +156,101 @@ window.countNQueensSolutions = function(n) {
     }
     solutions.push()
   }*/
+
+  // var solutions = [];
+  //  var makeEmptyMatrix = function(n) {
+  //   return _(_.range(n)).map(function() {
+  //     return _(_.range(n)).map(function() {
+  //       return 0;
+  //     });
+  //   });
+  // };
+  //  var recurse = function(board, i){
+  //   var skip = i;
+  //   for(var j = 1; j < n; j++){
+  //     if(i === board.get('n')-1){
+  //       i = 0;
+  //     }
+  //     board.toggle(j, i+1);
+  //     i++;
+  //   }
+  //   if(board)
+  //     solutions.push(board);
+  //     solutionCount++;
+  //   };
+  //   //if solution is valid AND pieces === n
+  // for(var i = 0; i < n; i++){
+  //   var board = makeEmptyMatrix(n);
+  //   board.toggle(0, i);
+  //   recurse(board, i);
+  // }
+  //
+  //   //make a storage for n (hypothetical) next boards
+      //loop though n times
+   /* for(var i = 0; i < n; i++){
+        //create a SINGLE next hypothetical board
+        // copy board onto hypo
+      var hypo = new Board({'n':n});
+      for(var a = 0; a < p; a++){
+        hypo.rows()[a]=board.rows()[a];
+      }
+      // add something new to hypo
+      hypo.togglePiece(p, i)
+      if(!hypo.hasAnyRookConflicts && p<n){
+        //check if it has conflicts
+        hypoStorage.push(hypo);
+      }else if(!hypo.hasAnyRookConflicts && p === n){
+          //none -> check if p === n
+            //push into solution storage
+          //put into local storage line 41
+        solutionStorage.push(hypo);
+        solutionCount++;
+      }
+    }*/
+  // var myBoard = new Board({'n':n});
+  // makeRookSolutions(myBoard, 0);
+  // for(var k = 0; k < hypoStorage.length; k++){
+  //   //loop through local storage and recursively call mRS(localS[], p+1);
+  //   for(var j = 0; j < hypoStorage[k].length; j++){
+  //     makeRookSolutions(hypoStorage[k], p);
+  //     p++;
+  //   }
+  // }
+  //     console.log(solutionStorage);
+  //
+  //
+  // if(n<3){
+  //   return n;
+  // }
+  // var solutionCount = 0;
+  // var solutions = [];
+
+  // var makeSolution = function(myBoard, row) {
+  //   if(row === n){
+  //     solutions.push(myBoard);
+  //     solutionCount++;
+  //     return;
+  //   }
+  //   for(var i = 0; i < n*n; i++){
+  //     if(i > n){
+  //       i = 0;
+  //     }
+  //     myBoard.togglePiece(row, i);
+  //     if(!myBoard["hasAnyRooksConflicts"]()){
+  //       row++;
+  //       makeSolution(myBoard, row);
+  //       var hold = myBoard.rows()[row];
+  //       myBoard.rows()[row-1] = hold;
+
+  //       return;
+  //     }
+  //     else{
+  //       myBoard.togglePiece(row, i);
+  //     }
+  //   }
+  // };
+  // for(var i = 0; i < n; i++){
+  //   var myBoard = new Board({'n':n});
+  //   myBoard.togglePiece(0, i);
+  //   makeSolution(myBoard, 1);//empty board with one rook on the top row
+  // }
